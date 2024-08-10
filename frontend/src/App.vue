d8569ce3e1d4f1915fe9844202a5baf26a31e58b
limjihoon99 <bo9701@naver.com>
2024-08-10 18:30:42 +0900

    feat: KakaoMap components 분리

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,42 @@
 <template>
-  <div>
-    <h3>Kakao Map Demo(center, level)</h3>
-    <div class="kmap" ref="map"></div>
-  </div>
+    <div>
+        <h3>Kakao Map Demo(center, level)</h3>
+        <KakaoMap class="kmap" :options="mapOption"/>
+    </div>
 </template>
 <script>
+import KakaoMap from './components/map/KakaoMap.vue';
 export default {
-  mounted() {
-    let kakao = window.kakao;
-    console.log(this.$refs.map);
-    
-    var container = this.$refs.map
-    var options = {
-      center: new kakao.maps.LatLng(37.497212875468755, 126.92761685591375),
-      level: 3,
-    };
+    components: {
+        KakaoMap,
+    },
+    data() {
+        return {
+            mapOption: {
+                center: {
+                    lat: 37.497212875468755,
+                    lng: 126.92761685591375,
+                },
+                level: 3,
+            },
+        };
+    },
+    mounted() {
+        // let kakao = window.kakao;
+        // console.log(this.$refs.map);
 
-    const mapInstance = new kakao.maps.Map(container, options);
-    console.log(mapInstance);
-    
-  },
+        // var container = this.$refs.map
+        // var options = {
+        //     center: new kakao.maps.LatLng(37.497212875468755, 126.92761685591375),
+        //     level: 3,
+        // };
+
+        // const mapInstance = new kakao.maps.Map(container, options);
+        // console.log(mapInstance);
+
+    },
 }
 </script>
 <style>
-.kmap {
-  width: 100%;
-  height: 600px;
-}
+
 </style>
---
+++ frontend/src/components/map/KakaoMap.vue
@@ -0,0 +1,31 @@
+<template>
+    <div ref="map"></div>
+</template>
+<script>
+export default {
+    props: ['options'],
+    mounted() {
+        let kakao = window.kakao;
+        console.log(this.$refs.map);
+
+        var container = this.$refs.map
+        // var options = {
+        //     center: new kakao.maps.LatLng(37.497212875468755, 126.92761685591375),
+        //     level: 3,
+        // };
+
+        const {center, level} = this.options;
+        const mapInstance = new kakao.maps.Map(container, {
+            center: new kakao.maps.LatLng(center.lat, center.lng),
+            level,
+        });
+        console.log(mapInstance);
+    },
+}
+</script>
+<style>
+.kmap {
+    width: 100%;
+    height: 600px;
+}
+</style>
