5caba9eb7952512b3517ac0f49e330b0f7450a1e
jongwon <growjong8802@gmail.com>
2024-08-18 23:51:57 +0900

    feat : 마커 정보를 통해 포토스팟 등록 기능 구현

--- frontend/src/views/LandingPages/Map/MapView.vue
+++ frontend/src/views/LandingPages/Map/MapView.vue
@@ -24,16 +24,46 @@
       </div>
     </div>
   </div>
+  <PostModal v-if="showModalPost" :location="selectPlaceName" :address="selectPlaceAddr" :x="selectX" :y="selectY"
+    @close="closeModalPost" />
 </template>
 
 <script>
+import { ref } from "vue";
 import KakaoMap from "@/components/map/KakaoMap.vue";
 import NavbarDefault from "../../../examples/navbars/NavbarDefault.vue";
+import PostModal from "../../../components/PostModal.vue";
 
 export default {
   components: {
     KakaoMap,
     NavbarDefault,
+    PostModal
+  },
+  setup() {
+    const showModalPost = ref(false);
+    const selectX = ref('');
+    const selectY = ref('');
+    const selectPlaceName = ref('');
+    const selectPlaceAddr = ref('');
+
+    const openModalPost = () => {
+      showModalPost.value = true;
+    };
+
+    const closeModalPost = () => {
+      showModalPost.value = false;
+    };
+
+    return {
+      showModalPost,
+      selectX,
+      selectY,
+      selectPlaceName,
+      selectPlaceAddr,
+      openModalPost,
+      closeModalPost
+    };
   },
   data() {
     return {
@@ -57,7 +87,7 @@ export default {
       const { kakao } = window;
 
       this.infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
-      
+
       this.clusterer = new kakao.maps.MarkerClusterer({
         map: vueKakaoMap.mapInstance,
         averageCenter: true,
@@ -124,7 +154,7 @@ export default {
       places.forEach((place, index) => {
         const placePosition = new kakao.maps.LatLng(place.y, place.x);
         const marker = this.addMarker(placePosition, index);
-        const itemEl = this.getListItem(index, place, placePosition); // Pass position
+        const itemEl = this.getListItem(index, place, placePosition);
 
         bounds.extend(placePosition);
 
@@ -138,6 +168,16 @@ export default {
 
         kakao.maps.event.addListener(marker, 'click', () => {
           this.centerMap(placePosition);
+        });
+
+        kakao.maps.event.addListener(marker, 'click', () => {
+          console.log(place.x, place.y, place.place_name, place.address_name);
+          this.selectX = place.x;  // X 좌표
+          this.selectY = place.y;  // Y 좌표
+          this.selectPlaceName = place.place_name; // 장소 명
+          this.selectPlaceAddr = place.address_name; // 주소
+          this.openModalPost(); // 함수 호출에 괄호 추가
+          console.log(this.showModalPost);
         });
 
         fragment.appendChild(itemEl);
@@ -237,8 +277,7 @@ export default {
     },
     centerMap(position) {
       this.map.setCenter(position);
-      this.map.setLevel(4);
-    }
+    },
   },
 };
 </script>
@@ -249,11 +288,13 @@ button {
   padding: 6px;
   background-color: #efefefdd;
   border-radius: 6px;
+
   &:hover {
     background-color: #ddd;
     border-color: #ddd;
     cursor: pointer;
   }
+
   &:active {
     background-color: #aaa;
     border-color: #aaa;
@@ -261,40 +302,51 @@ button {
 }
 
 .map-area {
-  margin:0;
-  padding:0;
-  font-family:'Malgun Gothic',dotum,'돋움',sans-serif;
-  font-size:12px;
-  a, a:hover, a:active {
-    color:#000;
+  margin: 0;
+  padding: 0;
+  font-family: 'Malgun Gothic', dotum, '돋움', sans-serif;
+  font-size: 12px;
+
+  a,
+  a:hover,
+  a:active {
+    color: #000;
     text-decoration: none;
   }
+
   display: flex;
   position: relative;
+
   .harbors {
     .harbor {
       padding: 10px;
       border: 1px solid transparent;
+
       &:hover {
         background-color: aliceblue;
         border-color: #6a9dff;
         cursor: pointer;
       }
+
       &:active {
         background-color: rgb(166, 197, 224);
         border-color: #4471c5;
       }
+
       &.active {
         background-color: rgb(253, 229, 150);
         border-color: rgb(211, 173, 3);
       }
+
       h4 {
         margin: 0;
       }
     }
   }
+
   .kmap {
     flex: 1 1 auto;
+
     .overlay-popup {
       background-color: #ffffffcc;
       box-shadow: 0 0 8px #0000004d, 0 0 1px 2px #00000022;
@@ -304,6 +356,7 @@ button {
       bottom: 44px;
       left: 50%;
       transform: translateX(-50%);
+
       h3 {
         margin: 0;
         padding: 8px;
@@ -313,10 +366,12 @@ button {
         font-weight: 400;
         font-size: 16px;
       }
+
       .addr {
         padding: 8px;
         white-space: break-spaces;
       }
+
       .close {
         color: black;
         position: absolute;
@@ -331,38 +386,171 @@ button {
       }
     }
   }
-  #menu_wrap {position:absolute;top:0;left:0;bottom:0;width:350px;margin:10px 0 30px 10px;margin-top:115px;padding:5px;overflow-y:auto;background:rgba(255, 255, 255, 0.7);z-index: 1;font-size:12px;border-radius: 10px;}
-  .bg_white {background:#fff;}
-  #menu_wrap hr {display: block; height: 1px;border: 0; border-top: 2px solid #5F5F5F;margin:3px 0;}
-  #menu_wrap .option{text-align: center;}
-  #menu_wrap .option p {margin:10px 0;}  
-  #menu_wrap .option button {margin-left:5px;}
-  #placesList li {list-style: none;}
-  #placesList .item {position:relative;border-bottom:1px solid #888;overflow: hidden;cursor: pointer;min-height: 65px;}
-  #placesList .item span {display: block;margin-top:4px;}
-  #placesList .item h5, #placesList .item .info {text-overflow: ellipsis;overflow: hidden;white-space: nowrap;}
-  #placesList .item .info{padding:10px 0 10px 55px;}
-  #placesList .info .gray {color:#8a8a8a;}
-  #placesList .info .jibun {padding-left:26px;background:url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_jibun.png) no-repeat;}
-  #placesList .info .tel {color:#009900;}
-  #placesList .item .markerbg {float:left;position:absolute;width:36px; height:37px;margin:10px 0 0 10px;background:url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png) no-repeat;}
-  #placesList .item .marker_1 {background-position: 0 -10px;}
-  #placesList .item .marker_2 {background-position: 0 -56px;}
-  #placesList .item .marker_3 {background-position: 0 -102px}
-  #placesList .item .marker_4 {background-position: 0 -148px;}
-  #placesList .item .marker_5 {background-position: 0 -194px;}
-  #placesList .item .marker_6 {background-position: 0 -240px;}
-  #placesList .item .marker_7 {background-position: 0 -286px;}
-  #placesList .item .marker_8 {background-position: 0 -332px;}
-  #placesList .item .marker_9 {background-position: 0 -378px;}
-  #placesList .item .marker_10 {background-position: 0 -423px;}
-  #placesList .item .marker_11 {background-position: 0 -470px;}
-  #placesList .item .marker_12 {background-position: 0 -516px;}
-  #placesList .item .marker_13 {background-position: 0 -562px;}
-  #placesList .item .marker_14 {background-position: 0 -608px;}
-  #placesList .item .marker_15 {background-position: 0 -654px;}
-  #pagination {margin:10px auto;text-align: center;}
-  #pagination a {display:inline-block;margin-right:10px;}
-  #pagination .on {font-weight: bold; cursor: default;color:#777;}
+
+  #menu_wrap {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 350px;
+    margin: 10px 0 30px 10px;
+    margin-top: 115px;
+    padding: 5px;
+    overflow-y: auto;
+    background: rgba(255, 255, 255, 0.7);
+    z-index: 1;
+    font-size: 12px;
+    border-radius: 10px;
+  }
+
+  .bg_white {
+    background: #fff;
+  }
+
+  #menu_wrap hr {
+    display: block;
+    height: 1px;
+    border: 0;
+    border-top: 2px solid #5F5F5F;
+    margin: 3px 0;
+  }
+
+  #menu_wrap .option {
+    text-align: center;
+  }
+
+  #menu_wrap .option p {
+    margin: 10px 0;
+  }
+
+  #menu_wrap .option button {
+    margin-left: 5px;
+  }
+
+  #placesList li {
+    list-style: none;
+  }
+
+  #placesList .item {
+    position: relative;
+    border-bottom: 1px solid #888;
+    overflow: hidden;
+    cursor: pointer;
+    min-height: 65px;
+  }
+
+  #placesList .item span {
+    display: block;
+    margin-top: 4px;
+  }
+
+  #placesList .item h5,
+  #placesList .item .info {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  #placesList .item .info {
+    padding: 10px 0 10px 55px;
+  }
+
+  #placesList .info .gray {
+    color: #8a8a8a;
+  }
+
+  #placesList .info .jibun {
+    padding-left: 26px;
+    background: url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_jibun.png) no-repeat;
+  }
+
+  #placesList .info .tel {
+    color: #009900;
+  }
+
+  #placesList .item .markerbg {
+    float: left;
+    position: absolute;
+    width: 36px;
+    height: 37px;
+    margin: 10px 0 0 10px;
+    background: url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png) no-repeat;
+  }
+
+  #placesList .item .marker_1 {
+    background-position: 0 -10px;
+  }
+
+  #placesList .item .marker_2 {
+    background-position: 0 -56px;
+  }
+
+  #placesList .item .marker_3 {
+    background-position: 0 -102px
+  }
+
+  #placesList .item .marker_4 {
+    background-position: 0 -148px;
+  }
+
+  #placesList .item .marker_5 {
+    background-position: 0 -194px;
+  }
+
+  #placesList .item .marker_6 {
+    background-position: 0 -240px;
+  }
+
+  #placesList .item .marker_7 {
+    background-position: 0 -286px;
+  }
+
+  #placesList .item .marker_8 {
+    background-position: 0 -332px;
+  }
+
+  #placesList .item .marker_9 {
+    background-position: 0 -378px;
+  }
+
+  #placesList .item .marker_10 {
+    background-position: 0 -423px;
+  }
+
+  #placesList .item .marker_11 {
+    background-position: 0 -470px;
+  }
+
+  #placesList .item .marker_12 {
+    background-position: 0 -516px;
+  }
+
+  #placesList .item .marker_13 {
+    background-position: 0 -562px;
+  }
+
+  #placesList .item .marker_14 {
+    background-position: 0 -608px;
+  }
+
+  #placesList .item .marker_15 {
+    background-position: 0 -654px;
+  }
+
+  #pagination {
+    margin: 10px auto;
+    text-align: center;
+  }
+
+  #pagination a {
+    display: inline-block;
+    margin-right: 10px;
+  }
+
+  #pagination .on {
+    font-weight: bold;
+    cursor: default;
+    color: #777;
+  }
 }
 </style>
