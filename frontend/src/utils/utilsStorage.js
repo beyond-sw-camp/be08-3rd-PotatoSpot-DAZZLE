3d3f4fe9a45323d87392340c135a8ac410055ed1
jongwon <growjong8802@gmail.com>
2024-08-18 23:52:49 +0900

    feat : 탭 간격 수정

--- frontend/src/utils/utilsStorage.js
+++ frontend/src/utils/utilsStorage.js
@@ -2,19 +2,19 @@ import { ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage
 import { storage } from '../firebase'; // firebase.js에서 가져온 storage 객체
 
 export const uploadImage = async (file) => {
-    if (!file) return;
+  if (!file) return;
 
-    // Firebase Storage에 저장할 참조 생성
-    const fileRef = storageRef(storage, `images/${file.name}`);
+  // Firebase Storage에 저장할 참조 생성
+  const fileRef = storageRef(storage, `images/${file.name}`);
 
-    try {
-        // 파일 업로드
-        const snapshot = await uploadBytes(fileRef, file);
+  try {
+    // 파일 업로드
+    const snapshot = await uploadBytes(fileRef, file);
 
-        // 업로드 완료 후 다운로드 URL 가져오기
-        const downloadURL = await getDownloadURL(snapshot.ref);
-        return downloadURL;
-    } catch (error) {
-        console.error("Error uploading file: ", error);
-    }
+    // 업로드 완료 후 다운로드 URL 가져오기
+    const downloadURL = await getDownloadURL(snapshot.ref);
+    return downloadURL;
+  } catch (error) {
+    console.error("Error uploading file: ", error);
+  }
 };
